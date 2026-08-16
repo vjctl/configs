4137ed17c45e61a0033052a69d6ce8e64b4b6a69
vjctl <108724896+vjctl@users.noreply.github.com>
2026-08-16 12:32:54 +0200

    nixified

--- nix/nix.sh
+++ nix/nix.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 if [[ "$(uname)" == "Darwin" ]]; then
   xcode-select --install
 fi
-printf 'Installing Nix\n'
-curl -fsSL https://install.determinate.systems/nix | sh -s -- install
+if ! command -v nix >/dev/null 2>&1; then
+  printf '\nInstalling Nix\n'
+  curl -fsSL https://install.determinate.systems/nix | sh -s -- install
+fi
 mkdir -p ~/.config/home-manager
 cp -r nix/flake.* nix/home.nix ~/.config/home-manager/
 printf '\nConfiguring Home Manager\n'
